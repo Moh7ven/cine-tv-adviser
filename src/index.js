3ea1a7207a00c9a9dcb43b4b5b3f91b3991a675a
Moh7ven <Mohamedsangare0107@gmail.com>
2024-04-17 14:32:11 +0000

    StrictMode added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 // import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  // Strict mode va exécuter deux fois mes uses effect en  developpement
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
