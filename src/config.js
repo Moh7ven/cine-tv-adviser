fde431d1489e44ca9f318770960cb280dec9517b
Moh7ven <Mohamedsangare0107@gmail.com>
2024-05-02 23:42:50 +0000

    comment

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 //api pour l'app
+//toutes les urls de base
 const BASE_URL = "https://api.themoviedb.org/3/";
 const API_KEY_PARAM = "?api_key=aaa32e0f4daf3fee9fae682b58596249";
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
